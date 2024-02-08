fcb87685f82848beb1f53118f3a10e53c3952ca9
Mauricio Quintero <mauri1987@gmail.com>
2024-02-07 21:54:58 -0300

    Exercise 4.2

---
+++ controllers/blogs.js
@@ -0,0 +1,21 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save()
+    .then(blog => {
+      response.status(201).json(blog)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter
